90ee658957990ffa0e34d108797584094ac7dc41
elijaholmos <eli.olmos9@gmail.com>
2022-10-12 07:19:48 -0700

    TOS frontend; missing functionality

--- src/popup/TermsOfService.svelte
+++ src/popup/TermsOfService.svelte
@@ -19,34 +19,37 @@
 </script>
 
 <div class="flex flex-col items-center">
-	<div class="alert alert-error shadow-lg text-base p-3 m-4">
+	<div class="alert shadow-lg text-base p-3 m-2 w-auto">
 		<div>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
-				class="stroke-current flex-shrink-0 h-6 w-6"
 				fill="none"
 				viewBox="0 0 24 24"
+				class="stroke-info flex-shrink-0 w-6 h-6"
 				><path
 					stroke-linecap="round"
 					stroke-linejoin="round"
 					stroke-width="2"
-					d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+					d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
 				/></svg
 			>
-			<slot class="text-center">
-				<span>You must accept the Terms of Service to use this application</span>
-			</slot>
+
+			<div class="text-center">
+				<span
+					>You must accept the <a
+						class="link link-hover text-info"
+						href="https://elijaho.notion.site/Terms-of-Service-e341190b0998499ea7f31cee2d49f786"
+						target="_blank"
+						>Terms of Service
+					</a> in order to use this application</span
+				>
+			</div>
 		</div>
 	</div>
-	<a href="https://discord.gg/mY563YHScv" target="_blank">
-		<button class="btn gap-2 bg-discord hover:bg-discord-hover m-2 p-2 text-white">
-			<img
-				class="h-6 w-6"
-				src="../static/Discord-Logo-White.svg"
-				srcset="../static/Discord-Logo-White.svg"
-				alt="Discord logo"
-			/>
-			Support
-		</button>
-	</a>
+
+	<div class="flex flex-col items-center [&>p]:w-11/12">
+		<p class="text-base">I have read and agree to the Terms of Service.</p>
+		<p class="text-base">I authorize Halo Notification Service to view my education records.</p>
+		<button class="btn btn-outline btn-primary w-24 m-4">Agree</button>
+	</div>
 </div>
