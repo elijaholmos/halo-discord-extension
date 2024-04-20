1d521294cedbdcd814bbee4716c710d133f08a77
Elijah Olmos <35435704+elijaholmos@users.noreply.github.com>
2024-04-19 21:30:41 -0700

    chore(license): update headers to 2024 (#173)

--- src/popup/components/LazyLoader.svelte
+++ src/popup/components/LazyLoader.svelte
@@ -1,5 +1,5 @@
 <!--
-  ~ Copyright (C) 2023 Elijah Olmos
+  ~ Copyright (C) 2024 Elijah Olmos
   ~
   ~ This program is free software: you can redistribute it and/or modify
   ~ it under the terms of the GNU Affero General Public License as
@@ -23,7 +23,6 @@
 
 	export let lazyLoad = () => {};
 </script>
-
 
 <!-- Removed wrapper div becayse styling errors occurred -->
 
